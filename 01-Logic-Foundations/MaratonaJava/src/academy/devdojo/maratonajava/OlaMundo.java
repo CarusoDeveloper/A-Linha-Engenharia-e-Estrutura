660857ca6942ac37f5cf03f725cf793a5e8e81dc
CarusoDeveloper <joaovictor3303gg@gmail.com>
2026-02-08 12:52:03 -0300

    feat: adicionando Maratona Java e organização de pastas

---
+++ 01-Logic-Foundations/MaratonaJava/src/academy/devdojo/maratonajava/OlaMundo.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava;
+
+public class OlaMundo {
+    //comentario teste
+    public static void main(String[] args) {
+        System.out.print("KA-ME-HA-ME-HAAAAAAAAAAAAAAAAAAAAAA");
+    }
+}
