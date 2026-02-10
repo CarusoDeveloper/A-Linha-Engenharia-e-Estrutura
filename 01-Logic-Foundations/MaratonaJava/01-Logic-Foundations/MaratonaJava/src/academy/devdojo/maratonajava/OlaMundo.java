493c707db475b4b585c80de71eb214e44e94992d
CarusoDeveloper <joaovictor3303gg@gmail.com>
2026-02-10 20:17:19 -0300

    Organizando as Pastas

--- 01-Logic-Foundations/MaratonaJava/01-Logic-Foundations/MaratonaJava/src/academy/devdojo/maratonajava/OlaMundo.java
+++
@@ -1,8 +0,0 @@
-package academy.devdojo.maratonajava;
-
-public class OlaMundo {
-    //comentario teste
-    public static void main(String[] args) {
-        System.out.print("KA-ME-HA-ME-HAAAAAAAAAAAAAAAAAAAAAA");
-    }
-}
