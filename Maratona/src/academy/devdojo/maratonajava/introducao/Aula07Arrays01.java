f40cf51236acef33952c42ebbc7a3e699ff885cc
CarusoDeveloper <joaovictor3303gg@gmail.com>
2026-02-12 21:26:48 -0300

    build: limpa o repositório e mantém apenas arquivos base

--- Maratona/src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
+++
@@ -1,9 +0,0 @@
-package academy.devdojo.maratonajava.introducao;
-
-public class Aula07Arrays01 {
-    public static void main(String[] args) {
-        int [] idades = new int[3];
-
-        System.out.println(idades[0]);
-    }
-}
